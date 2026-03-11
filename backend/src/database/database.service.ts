f16645e4d36e48386aad30fb87c22c68b6f509e0
Lopepi <lopepi@outlook.fr>
2026-03-11 15:39:34 +0100

    chore/ create databse service

---
+++ backend/src/database/database.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Pool, Poolconfig } from 'pg';
+
+@Injectable()
+export class DatabaseService implements OnModuleInit, OnModuleDestroy {
+
+    private pool: Pool;
+
+    // Postgres configuration
+    private readonly config: Poolconfig = {
+       user: process.env.DB_USER,
+       password: process.env.DB_PASSWORD,
+       host: process.env.DB_HOST,
+       database: process.env.DB_NAME,
+       port: Number(process.env.DB_PORT),
+       pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+       }
+    };
+
+    // Connectiong to DB when app laucnh
+    async onModuleInit(): Promise<void> {
+        await this.connectToDatabase();
+    }
+
+    // Closing connection to DB when app stop
+    async onModuleDestroy(): Promise<void> {
+        if (this.pool) {
+            await this.pool.close();
+        }
+    }
+
+    // Function called to connect to DB
+    private async connectToDatabase(): Promise <void> {
+        try{
+            if (!this.pool) {
+                this.pool = await new Pool(this.config).connect();
+                console.log('DB connected.')
+            }
+        } catch (err) {
+            throw new Error('Connection to DB failed.')
+        }
+    }
+}
