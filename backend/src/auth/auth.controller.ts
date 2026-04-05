4daf8739959d9deb36c8d7ddc8ed61e033180191
Lopepi <lopepi@outlook.fr>
2026-04-05 17:42:12 +0200

    Fixed minor authentication bug

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthBody } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor (private readonly authService : AuthService) {}
+
+    @Post()
+    loginUser(@Body() authBody: AuthBody) {
+        return this.authService.loginUser({authBody});
+    }
+}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
+import { hash, compare } from 'bcrypt';
+
+export type AuthBody = {login : string, password : string};
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private readonly databaseService: DatabaseService) {}
+
+    async loginUser({authBody} : {authBody : AuthBody}) {
+
+        const {login, password} = authBody;
+
+        const hashedPassword = await this.hashPassword(password);
+
+        console.log({ hashedPassword, password });
+
+        const query = `SELECT * FROM users WHERE login = $1`;
+
+        const result = await this.databaseService.runQuery(query, [login]);
+
+        const existingUser = result.rows?.[0];
+
+        if(!existingUser) {
+            throw new Error("L'utilisateur n'existe pas.");
+        }
+
+        const isPasswordValid = await this.isPasswordValid(password, existingUser.password);
+
+        return existingUser;
+    }
+
+    private async hashPassword(password : string) {
+
+        const hashedPassword = await hash(password, 10);
+
+        return hashedPassword;
+    }
+
+    private async isPasswordValid(password : string, hashedPassword : string) {
+
+        const isPasswordValid = await compare(password, hashedPassword);
+
+        return isPasswordValid;
+    }
+}
