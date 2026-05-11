29b93ade471caed6cae88a9b42d6b305ceaa8b15
Lopepi <lopepi@outlook.fr>
2026-05-11 22:12:33 +0200

    Fixed angular server error

---
+++ frontend/src/app/users-interface/users-interface.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-interface',
+  standalone: true,
+  imports: [],
+  templateUrl: './users-interface.component.html',
+  styleUrl: './users-interface.component.scss'
+})
+export class UsersInterfaceComponent {
+
+}
